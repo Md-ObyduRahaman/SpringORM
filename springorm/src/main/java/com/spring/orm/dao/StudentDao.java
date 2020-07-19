2b611d24914f70a135ff993d5df7d584ea4c6cff
USER <sojib.19991018@gmail.com>
2020-07-19 15:55:51 +0600

    Complete Configuration of Spring ORM step by step in XML

--- springorm/src/main/java/com/spring/orm/dao/StudentDao.java
+++ springorm/src/main/java/com/spring/orm/dao/StudentDao.java
@@ -1,5 +1,7 @@
 package com.spring.orm.dao;
 
+import javax.transaction.Transactional;
+
 import org.springframework.orm.hibernate5.HibernateTemplate;
 
 import com.spring.orm.entities.Student;
@@ -8,9 +10,18 @@ public class StudentDao {
 	private HibernateTemplate hibernateTemplate;
 
 	// save student
+	@Transactional
 	public int insert(Student student) {
 		Integer i = (Integer) this.hibernateTemplate.save(student);
 		return i;
+	}
+
+	public HibernateTemplate getHibernateTemplate() {
+		return hibernateTemplate;
+	}
+
+	public void setHibernateTemplate(HibernateTemplate hibernateTemplate) {
+		this.hibernateTemplate = hibernateTemplate;
 	}
 
 }
--- springorm/src/main/java/com/spring/orm/springorm/App.java
+++ springorm/src/main/java/com/spring/orm/springorm/App.java
@@ -1,13 +1,22 @@
 package com.spring.orm.springorm;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.spring.orm.dao.StudentDao;
+import com.spring.orm.entities.Student;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+
+		ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+		StudentDao studentDao = context.getBean("studentDao", StudentDao.class);
+		Student student = new Student(1194, "SOjib", "Mymensingh");
+		int insert = studentDao.insert(student);
+		System.out.println("done....." + insert);
+	}
 }
